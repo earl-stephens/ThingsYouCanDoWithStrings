4d7f942eb362b19e96f708b5dcc5174118ff172e
earl-stephens <stephensearl@hotmail.com>
2021-12-14 16:45:39 -0700

    Added code for concatenating strings

--- src/application/App.java
+++ src/application/App.java
@@ -3,7 +3,15 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String name = "John";
+		// "John" is a string literal
+		
+		System.out.println("My name is " + name);
+		
+		String text = "I am called: ";
+		String endOfSentence = ".";
+		
+		System.out.println(text + name + endOfSentence);
 
 	}
 
